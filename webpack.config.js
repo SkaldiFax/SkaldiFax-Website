b68d6ba5a1db2fe24ddacf7b6498f88181377b6f
SkaldiFax <skaldi.fax@gmail.com>
2018-02-18 19:21:31 +0000

    Setup webpack, attempt to integrate SASS compilation.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-es2015']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    }, 
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
